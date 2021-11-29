3c98871d80f66b82ddf5592f58e7aaf7e910e1b9
Federico Clavero <federico.j.clavero@gmail.com>
2021-11-29 12:52:57 +0000

    Python template

---
+++ README.md
@@ -0,0 +1,6 @@
+Interview Problem 01
+======
+![Python package](https://github.com/fclav/hello-world-python/workflows/Python%20package/badge.svg)
+
+
+This is a Python template for new packages.
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hello_world_python',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['''
+    '''],
+    entry_points='''
+    ''',
+)
